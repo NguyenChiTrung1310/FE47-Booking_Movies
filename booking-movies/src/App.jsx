30b3082c47d9610e87954ea16388f991af03288f
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-09-27 11:44:15 +0700

    [HM-1]: Create new branch git

--- booking-movies/src/App.jsx
+++ booking-movies/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { LoginPage } from './pages/Login/LoginPage';
 
 function App() {
-  return <div className='App'>Hello Movies</div>;
+  return (
+    <div className='App'>
+      Hello Movies <LoginPage />
+    </div>
+  );
 }
 
 export default App;
