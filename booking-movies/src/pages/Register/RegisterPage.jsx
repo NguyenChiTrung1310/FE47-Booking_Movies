867f64016ccbf32a11698dd396a9b9ed08251e86
Hai Vu <duchaivu1997@gmail.com>
2020-10-03 16:39:06 +0700

    [HM-2]:Remove formik+ fix margin top register form

--- booking-movies/src/pages/Register/RegisterPage.jsx
+++ booking-movies/src/pages/Register/RegisterPage.jsx
@@ -1,32 +1,24 @@
-import React   from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Container, TextField, Typography, Grid } from '@material-ui/core';
-import { Formik, Form,   ErrorMessage } from 'formik';
+import { Container, TextField, Typography } from '@material-ui/core'; 
 import './RegisterPage.scss';
 import { useStyles } from './../Register/useStyles';
-import Button from './../../components/Button/Button';
-import * as yup from 'yup';
-
-const registerSchema = yup.object().shape({
-  account: yup.string().required('Account must be fill out').nullable(),
-  password: yup.string().required('Password must be fill out').nullable(),
-  fullname: yup.string().required('Full Name must be fill out').nullable(),
-  email: yup
-    .string()
-    .required('Email must be fill out')
-    .email('Email is invalid')
-    .nullable(),
-  phoneNumber: yup
-    .string()
-    .required('Phone Number must be fill out')
-    .matches(/^[0-9]+$/),
-});
-
+import Button from './../../components/Button/Button'; 
 const RegisterPage = () => {
   const classes = useStyles();
 
+  const [account, setaccount] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setphoneNumber] = useState('');
+
   const handleSubmit = (e) => {
-    console.log(e);
+    e.preventDefault();
+
+    console.log(
+      `Account: ${account} Password:${password} Fullname:${fullname} Email: ${email} and password: ${password} Phone Number: ${phoneNumber}`
+    );
   };
   return (
     <Container
@@ -41,113 +33,90 @@ const RegisterPage = () => {
         >
           Register
         </Typography>
-        <Formik
-          initialValues={{
-            account: '',
-            password: '',
-            fullname: '',
-            email: '',
-            phoneNumber: '',
-          }}
+        <form
+          className={classes.form}
+          noValidate
           onSubmit={handleSubmit}
-          render={(formikProps) => (
-            <Form
-              className={classes.form}
-              noValidate
-            >
-              <TextField
-                autoComplete='account'
-                autoFocus
-                className={classes.field}
-                fullWidth
-                id='account'
-                label='Account'
-                margin='normal'
-                name='account'
-                onChange={formikProps.handleChange}
-                required
-                type='text'
-                variant='outlined'
-              />
-              <ErrorMessage name='account' />
-
-              <TextField
-                autoComplete='password' 
-                className={classes.field}
-                fullWidth
-                id='password'
-                label='Password'
-                margin='normal'
-                name='password'
-                onChange={formikProps.handleChange}
-                required
-                type='password'
-                variant='outlined'
-              />
-              <ErrorMessage name='password' />
-              <TextField
-                autoComplete='fullname' 
-                className={classes.field}
-                fullWidth
-                id='fullname'
-                label='Full Name'
-                margin='normal'
-                name='fullname'
-                onChange={formikProps.handleChange}
-                required
-                type='text'
-                variant='outlined'
-              />
-              <ErrorMessage name='fullname' />
-              <TextField
-                autoComplete='email' 
-                className={classes.field}
-                fullWidth
-                id='email'
-                label='Email'
-                margin='normal'
-                name='email'
-                onChange={formikProps.handleChange}
-                required
-                type='email'
-                variant='outlined'
-              />
-              <ErrorMessage name='email' />
-              <TextField
-                autoComplete='phoneNumber' 
-                className={classes.field}
-                fullWidth
-                id='phoneNumber'
-                label='Phone Number'
-                margin='normal'
-                name='phoneNumber'
-                onChange={formikProps.handleChange}
-                required
-                type='text'
-                variant='outlined'
-              />
-              <ErrorMessage name='phoneNumber' />
-
-              <Button type='submit'>Create Account</Button>
-              <Grid container>
-                <Grid item>
-                  <div className={classes.loginLink}>
-                    <p>
-                  Have an account?
-                      <a
-                        className={classes.link}
-                        href='#login'
-                      >
-                    Login
-                      </a>
-                    </p>
-                  </div>
-                </Grid>
-              </Grid>
-            </Form>
-          )}
-          validationSchema={registerSchema}
-        />
+        >
+          <TextField
+            autoComplete='account'
+            autoFocus
+            className={classes.field}
+            fullWidth
+            id='account'
+            label='Account'
+            margin='normal'
+            name='account'
+            onChange={evt => setaccount(evt.target.value)}
+            required
+            type='text'
+            value={account}
+            variant='outlined'
+          />
+          <TextField
+            autoComplete='password' 
+            className={classes.field}
+            fullWidth
+            id='password'
+            label='Password'
+            margin='normal'
+            name='password'
+            onChange={evt => setPassword(evt.target.value)}
+            required
+            type='password'
+            value={password}
+            variant='outlined'
+          />
+          <TextField
+            autoComplete='fullname' 
+            className={classes.field}
+            fullWidth
+            id='fullname'
+            label='Fullname'
+            margin='normal'
+            name='fullname'
+            onChange={evt => setFullname(evt.target.value)}
+            required
+            type='text'
+            value={fullname}
+            variant='outlined'
+          />
+          <TextField
+            autoComplete='email' 
+            className={classes.field}
+            fullWidth
+            id='email'
+            label='Email'
+            margin='normal'
+            name='email'
+            onChange={evt => setEmail(evt.target.value)}
+            required
+            type='text'
+            value={email}
+            variant='outlined'
+          />
+          <TextField
+            autoComplete='phomeNumber' 
+            className={classes.field}
+            fullWidth
+            id='phoneNumber'
+            label='Phone Number'
+            margin='normal'
+            name='phoneNumber'
+            onChange={evt => setphoneNumber(evt.target.value)}
+            required
+            type='text'
+            value={phoneNumber}
+            variant='outlined'
+          />
+          <Button
+            color='primary'
+            type='submit'
+            variant='contained'
+          >
+            Create Account
+          </Button>
+        </form>
       </div>
     </Container>
   );
@@ -157,4 +126,5 @@ RegisterPage.propTypes = {
   onChange: PropTypes.func,
 };
 
-export default RegisterPage;
+export default RegisterPage ;
+
