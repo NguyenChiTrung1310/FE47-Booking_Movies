24f3d31c418960b42dae98c8d726ca73149db180
NguyenChiTrung1310 <65656118+NguyenChiTrung1310@users.noreply.github.com>
2020-10-11 14:31:04 +0700

    Revert "Feature/hm 7 home page"

--- booking-movies/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import LoginPage from './Login/LoginPage';
-import RegisterPage from './Register/RegisterPage';
-import ErrorPage from './Error/ErrorPage';
-import HomePage from './Home/HomePage';
-import NewsPage from './News/News';
-import ContactPage from './Contact/Contact';
-
-export {LoginPage,RegisterPage,ErrorPage, HomePage, NewsPage, ContactPage} 
