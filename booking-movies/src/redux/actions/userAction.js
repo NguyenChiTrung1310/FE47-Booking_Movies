d5fd6ad9eb77c24bae43262b5c24e11fba5b08cd
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-10-18 19:15:35 +0700

    [HM-9]: 3) In action file, create functions success and failed

--- booking-movies/src/redux/actions/userAction.js
+++ booking-movies/src/redux/actions/userAction.js
@@ -1,4 +1,24 @@
+import { LOGIN_FAILL, LOGIN_SUCCESS } from '../../constants/constant';
 import {LoginService} from '../../services';
+
+// login success
+const loginSucceeded = (loginData) => {
+  const {status} = loginData;
+  return {
+    type: LOGIN_SUCCESS,
+    loginStatus: status, //200
+    // loginMessage: msg
+  };
+}
+
+// login failed
+const loginFailed = (loginData) => {
+  const {status} = loginData;
+  return {
+    type: LOGIN_FAILL,
+    loginStatus: status
+  };
+}
 
 export const loginAction = (taiKhoan, matKhau) => {
   return (dispatch) => {
