0960ed68b55c9ae75ac82f4afb5dde4b953b91dc
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-10-24 10:48:08 +0700

    [HM-10]: 1) Add api and profile service

--- booking-movies/src/services/api.js
+++ booking-movies/src/services/api.js
@@ -1,4 +1,5 @@
 const LOGIN_API = 'https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap';
 const REGISTER_API = 'https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangKy';
+const USER_PROFILE_API = 'https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThongTinTaiKhoan';
 
-export {LOGIN_API, REGISTER_API};
+export {LOGIN_API, REGISTER_API, USER_PROFILE_API};
