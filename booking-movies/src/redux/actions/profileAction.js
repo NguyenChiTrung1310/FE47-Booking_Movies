2b458534301a85abe3b5aec39647a9f769c383c1
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-10-24 10:52:13 +0700

    [HM-10]: 3) Create a fucntion localstorage and action for Profile

---
+++ booking-movies/src/redux/actions/profileAction.js
@@ -0,0 +1,27 @@
+import { USER_PROFILE } from '../../constants/constant';
+import { ProfileService } from '../../services/profile';
+import { storeProfile } from '../../utils/LocalStorage/LocalStorage';
+
+const profile = (userData) => {
+  const {data} = userData;
+  return {
+    type: USER_PROFILE,
+    payload: data
+  }
+}
+
+export const inforUserAction = (taiKhoan) => {
+  return (dispatch) => {
+    ProfileService(taiKhoan)
+      .then(res => {
+        const {data} = res;
+        dispatch(profile(res));
+        storeProfile(JSON.stringify(data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
+
+
