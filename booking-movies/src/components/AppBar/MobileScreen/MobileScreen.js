b571a7fde40acf48cf6bd59a10b6371a8aea7af2
Trung Nguyen <nguyenchitrung1310@gmail.com>
2020-12-06 18:02:41 +0700

    Refractoring function and optimizing source code

---
+++ booking-movies/src/components/AppBar/MobileScreen/MobileScreen.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { IconButton, Toolbar, Typography, Drawer } from '@material-ui/core'; 
+import {useStyles} from '../useStyles';
+import LinkMenu from '../LinkMenu/LinkMenu';
+import IconUser from '../IconUser/IconUser';
+import PropTypes from 'prop-types';
+
+
+const MobileScreen = ({drawerOpen, setState}) => {
+  const classes = useStyles();
+
+  const handleDrawerOpen=()=>{
+    setState((prevState)=> ({...prevState, drawerOpen: true}));
+  }
+  const handleDrawerClose= ()=>{
+    setState((prevState)=> ({...prevState, drawerOpen: false}));
+  }
+  
+  return (
+    <Toolbar className={classes.toolBar}>
+      <IconButton
+        aria-haspopup= 'true'
+        aria-label='menu'
+        className={classes.menuButton}
+        color='inherit'
+        edge='start'
+        onClick= {handleDrawerOpen} 
+      >
+        <span className={classes.brand}>Cinnema <span className={classes.plusIcon}><AddIcon fontSize='inherit'/></span></span>
+      </IconButton>
+           
+      <Drawer
+        {...{
+          anchor: 'left',
+          open: drawerOpen,
+          onClose: handleDrawerClose,  
+          width:'10px'
+        }}
+      >
+        <Typography
+          className={classes.menuLinkss} 
+          variant='h6'
+        >
+          <div className={classes.linkss}>
+            <LinkMenu />
+          </div>
+        </Typography> 
+      </Drawer>
+      <IconUser />
+    </Toolbar>  
+  )
+}
+
+MobileScreen.propTypes = {
+  drawerOpen: PropTypes.bool,
+  setState: PropTypes.func,
+}
+
+export default MobileScreen
